b86d8afb902a32240def991ac86e84ccd72d5105
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-29 04:02:47 +0000

    worked on Category class

--- lib/best_books/category.rb
+++ lib/best_books/category.rb
@@ -6,8 +6,20 @@ class BestBooks::Category
     #should return each category instance
     puts <<-DOC.gsub /^\s*/,''
       1. Fiction
-      2. Horror
+      2. Mystery & Thriller
     DOC
+
+    fiction = Category.new
+    fiction.name = "Fiction"
+    fiction.book = "Still Me (Me Before You #3)"
+    fiction.url = "https://www.goodreads.com/book/show/35791968-still-me?from_choice=true"
+
+    mystery = Category.new
+    mystery.name = "Horror"
+    mystery.book = "The Outsider"
+    mystery.url = "https://www.goodreads.com/choiceawards/best-mystery-thriller-books-2018"
+
+    [fiction, mystery]
   end
 
 end
