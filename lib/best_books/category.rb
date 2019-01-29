d93b756a4c9abafc07377a5c82ee89384bea504f
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-29 04:03:32 +0000

    added Category attr_accessors

--- lib/best_books/category.rb
+++ lib/best_books/category.rb
@@ -1,6 +1,7 @@
 #These are the categories Goodreads uses to sort their Best Books by genre
 
 class BestBooks::Category
+  attr_accessor :name, :book, :url
 
   def self.all
     #should return each category instance
