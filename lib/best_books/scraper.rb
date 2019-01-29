7153ae1d2d67418e4dfabd6dfb3f4674390f0a69
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-29 01:51:28 +0000

    worked on scraper

--- lib/best_books/scraper.rb
+++ lib/best_books/scraper.rb
@@ -1,26 +1,42 @@
 #This file is to pull TITLE and CATEGORY from https://www.goodreads.com/choiceawards/best-books-2018
-#This file should also pull AUTHOR and DESCRIPTION from the url for each top book 
+#This file should also pull AUTHOR and DESCRIPTION from the url for each top book
 
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
 class BestBooks::Scraper
-  
-  def self.scrape_books(goodreads_page)
-    doc = Nokogiri::HTML(open(goodreads_page))
-    categories = [] 
-    doc.css(right container).each do |container|
+
+  def get_page
+    Nokogiri::HTML(open("https://www.goodreads.com/choiceawards/best-books-2018"))
+  end
+
+  def self.scrape_books
+    books = []
+    self.get_page.css("div.categoryContainer").each do |book_box|
+      book_box.css(".category.clearFix a").each do |book|
+        category = book.css(".category__copy").text
+        title = book.css(".category__winnerImage").each{|img| puts img["alt"]}
+        book_url = "#{book.attr('href')}"
+        books << {category: category, title: title, book_url: book_url}
+      end
+    end
+    books
+  end
+
+
+
+
       category = container.css(category html)
       title = container.css(title html)
       book_url = "#{container.attr('href')}"
       books << {category: category, title: title, url: url}
-    end 
-    books 
-  end 
-  
-  
-  
-  
-  
-end 
+    end
+    books
+  end
+
+
+
+
+
+end
