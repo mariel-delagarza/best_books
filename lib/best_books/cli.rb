2a770682bcde6e62a5ebc31cb164d5aed13bc395
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-29 03:27:09 +0000

    worked on CLI.rb

--- lib/best_books/cli.rb
+++ lib/best_books/cli.rb
@@ -6,6 +6,8 @@ class BestBooks::CLI
 
   def call
     puts "Welcome to the Goodreads Best Books of 2018!"
+    list_categories
+    menu
     #start
   end
 
@@ -13,5 +15,31 @@ class BestBooks::CLI
   #  menu
   #  goodbye
   #end
+
+  def list_categories
+    puts ""
+    puts "Below is a list of the categories."
+    puts <<-DOC.gsub /^\s*/,''
+      1. Fiction
+      2. Horror
+    DOC
+  end
+
+  def menu
+    puts ""
+    puts "To see the top book in each category, enter the number of the category."
+  end
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
