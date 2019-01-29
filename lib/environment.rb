f58bbf68233622ad2598e14ac4165654c3ca291b
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-29 03:06:06 +0000

    changed environment file

--- lib/environment.rb
+++ lib/environment.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 
 
-require_relative "./best_books/version"
-require_relative "./best_books/book"
-require_relative './best_books/cli'
-require_relative './best_books/category'
+#require_relative "./best_books/version"
+#require_relative "./best_books/book"
+#require_relative './best_books/cli'
+#require_relative './best_books/category'
